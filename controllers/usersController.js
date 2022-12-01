a9ddc39448251b975559253b15604a09b611fc0d
William Stock <grim118@hotmail.com>
2022-12-01 16:49:21 -0600

    added controller functions for GET all users and CREATE new user along with the user routes

---
+++ controllers/usersController.js
@@ -0,0 +1,71 @@
+const User = require('../models/User')
+const Note = require('../models/Note')
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcrypt')
+
+// @desc GET all users
+// @route GET /users
+// @access Private
+const getAllUsers = asyncHandler(async (req, res) => {
+    const user = await User.find().select('-password').lean()
+    if (!users) {
+        return res.status(400).json({ message: 'No users found'})
+    }
+    res.json(users)
+})
+
+// @desc Create all users
+// @route POST /users
+// @access Private
+const createNewUser = asyncHandler(async (req, res) => {
+    const { username, passowrd, roles } = req.body
+
+    // Confirm data
+    if (!username || !passowrd || !Array.isArray(roles) || !roles.length) {
+        return res.status(400).json({ message: 'All fields are required'})
+    }
+
+    // check for duplicate
+    const duplicate = await User.findOne({ username }).lean().exec()
+
+    if (duplicate) {
+        return res.status(409).json({ message: 'Duplicate username'})
+    }
+
+    // hash password
+    const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
+
+    const userObject = { username, "password": hashedPwd, roles }
+
+    // create and store new user
+    const user = await User.create(userObject)
+
+    if (user) {
+        res.status(201).json({ message: `New user ${username} created`})
+    } else {
+        res.status(400).json({ message: 'Inavlid user data recieved' })
+    }
+})
+
+// @desc Update a user
+// @route PATCH /users
+// @access Private
+
+const updateUser = asyncHandler(async (req, res) => {
+
+})
+
+// @desc Delete a user
+// @route DELETE /users
+// @access Private
+const deleteUser = asyncHandler(async (req, res) => {
+
+})
+
+
+module.exports = {
+    getAllUsers,
+    createNewUser,
+    updateUser,
+    deleteUser
+}
